575f3325fca13b9c6a4b208455e4acad19661bfb
Дмитрий Лашин <lashinda@my.msu.ru>
2026-09-03 21:24:53 +0300

    Исправление config.py для запуска без .env

--- app/config.py
+++ app/config.py
@@ -2,11 +2,11 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 load_dotenv(os.path.join(basedir, '../.env'))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-fallback-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24).hex()
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://paleo_admin:paleo_secure_pass@db:5432/paleocolor_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ADMIN_SWATCH = 'cerulean'
